e198775d9559742e6ef0901d8dea2f61088c60e9
GiovanniGallinaro <giovanni_gallinaro@libero.it>
2019-06-04 11:19:07 +0200

    Fixed url login

--- src/main/java/it/unipd/dei/clef/servlet/LoginServlet.java
+++ src/main/java/it/unipd/dei/clef/servlet/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.net.URL;
 
 public class LoginServlet extends AbstractDatabaseServlet {
 
@@ -16,6 +17,10 @@ public class LoginServlet extends AbstractDatabaseServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
       request.getSession().setAttribute("email", request.getParameter("email"));
       request.getSession().setAttribute("password", request.getParameter("password"));
+	  
+	  String referrer = request.getHeader("Referer");
+	  URL ref = new URL(referrer);
+      referrer = ref.getPath().substring(request.getContextPath().length());
 
       ClefUser clefUser = new ClefUser(request.getParameter("email"), request.getParameter("password"));
 
@@ -23,7 +28,7 @@ public class LoginServlet extends AbstractDatabaseServlet {
         LoginDatabase log_db = new LoginDatabase(getDataSource().getConnection(), clefUser);
         request.getSession().setAttribute("log", log_db.Login());
         request.getSession().setAttribute("email", log_db.getUserEmail());
-        request.getRequestDispatcher("/jsp/home.jsp").forward(request, response);
+		request.getRequestDispatcher(referrer).forward(request, response);
       }
       catch (SQLException ex) {
         request.getRequestDispatcher("/jsp/home.jsp").forward(request, response);

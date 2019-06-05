1e29fe4b8856fe7221f3ca1bc5593928137d8ba7
Alessandro Cattapan <alecattapan22@gmail.com>
2019-06-05 10:52:02 +0200

    Completed

--- src/main/webapp/js/active.js
+++ src/main/webapp/js/active.js
@@ -6,8 +6,12 @@ var i;
 for(i = 0; i < links.length; i++)	{
 	
 	var element = links[i];
-	if(elHref[elHref.length-1] === url[url.length-1])	{
+	if(elHref[elHref.length-1] === url[url.length-1]){
 		element.className += " active";
 	}
 	
+}
+
+if(url[url.length-1] == "login"){
+	links[0].className += " active";
 }
